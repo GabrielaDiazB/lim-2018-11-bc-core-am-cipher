beb0a4a39a3aa609d6386afdcd8fe69ee4585fdc
GabrielaDiazB <gabriela.dbravo@gmail.com>
2018-11-19 23:11:24 -0500

    Cambio de un input en el html, css terminado, cambio de la función if por un for en JS.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 let n; // n es el offset
 let i; //letra por letra
-let texto;
-let string = ["ABC"]; 
-let codigo = [];
+let texto;//primer texto que es ingresado
+let string = []; //aquí se va a guardar push del texto en ascii
+let codigo = []; //
 let nuevoCodigo = []; //aquí rebotará el código
 
+n = document.getElementById("offsetBox").value;
+texto = document.getElementById("textBox").value;
 //función que codificará el texto
-function cifradoDeTexto()
-{
-    for(i=0; i<string.length; i++){
-        string.push((texto.charCodeAt(i)-65+1)%26+65);
-        return string;
+function cifradoDeTexto(){
+    for(i=0; i<texto.length; i++){
+        string.push((texto.charCodeAt(i)-65+n)%26+65);
+        if (texto.charCodeAt(i) >= 65 && texto.charCodeAt(i) >= 77) {
+            codigo.push(string.fromCharcodeAt(texto.charCodeAt(i)))
+        };    
     };
-    //if (condition) {} else {};
+    nuevoCodigo = codigo.join(" ");
+    document.getElementById("resultsBox").innerHTML = (nuevoCodigo.toUpperCase());
+   
+     
+
 };
 //función que decodificará el texto
 function decifradoDeTexto(){}
