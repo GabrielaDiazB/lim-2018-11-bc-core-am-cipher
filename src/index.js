40fe4192f20fc111c1736061139292b6154b8685
GabrielaDiazB <gabriela.dbravo@gmail.com>
2018-11-20 18:43:01 -0500

    nuevos cambios de JS en la separación de lo funcional de los eventos y cambios de divs en el index.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,47 @@
 
-//función que codificará el texto
+
+function cifradoDeTexto(offset,texto){
+    let stringAscii=[];
+    let codigo=[];
+    
+
+    for(let i=0; i<texto.length; i++){
+        texto.toUpperCase();
+        stringAscii = texto.charCodeAt(i);
+
+    };
+    if (stringAscii>=65 && stringAscii<=90) {
+        ((stringAscii-65+offset)%26+65);
+        codigo.push(stringAscii.fromCharcodeAt(texto.charCodeAt(i)));
+    };
 
 
-function cifradoDeTexto(){
+};
+function decifradoDeTexto(offset,texto){
+
+
+    for(let j=0; j<texto.length;j++){
+        
+    };
+    if (stringAscii>=65 && stringAscii<=90){
+
+        
+    }
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+/*function cifradoDeTexto(){
     let n; // n es el offset
     let texto = '' ;//primer texto que es ingresado
     let stringAscii = [] //aquí se va a guardar push del texto en ascii
@@ -24,13 +63,9 @@ function cifradoDeTexto(){
     console.log(codigo)
 
 
-    /*nuevoCodigo = nuevoCodigo.concat(texto[i]);
+    nuevoCodigo = nuevoCodigo.concat(texto[i]);
     document.getElementById("resultsBox").innerHTML = (nuevoCodigo.toUpperCase());*/
 
 };
 
-//función que decodificará el texto
-//function descifradoDeTexto(){}
-//Eventos para dar mostrar u ocultar divs
 
-//Eventos para dar funcionabilidad a los botones
