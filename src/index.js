814df912843b3d3cd16d118fb965148399359213
GabrielaDiazB <gabriela.dbravo@gmail.com>
2018-11-21 12:59:31 -0500

    cambios de evento en el js

--- src/index.js
+++ src/index.js
@@ -5,23 +5,24 @@ button1.addEventListener('click', cifradoDeTexto);
 const button2 = document.getElementById("decifradoDeTexto");
 button2.addEventListener('click', decifradoDeTexto);
 
+
 function cifradoDeTexto(){
     let offset = document.getElementById("offsetBox").value;
     let texto = document.getElementById("textBox").value.toUpperCase();
     let stringAscii=[];
     let codigo=[];
     let nuevoCodigo=[];
-
     for(let i=0; i<texto.length; i++){
 
         if (texto.charCodeAt(i)>=65 && texto.charCodeAt(i)<=90) {
             stringAscii.push(((texto.charCodeAt(i)-65+parseInt(offset))%26 + 65));
             codigo.push(String.fromCharCode(stringAscii[i]));
-        };
+        } else ()
+        
     };
     nuevoCodigo = codigo.join("");
-    console.log(nuevoCodigo);
-};
+    console.log(nuevoCodigo)
+}; 
 
 function decifradoDeTexto(){
     let offset = document.getElementById("offsetBox").value;
