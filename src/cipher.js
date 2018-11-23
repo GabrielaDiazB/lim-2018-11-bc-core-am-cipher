142e824843c3cfd675b81999e73ddc65c21c71e1
GabrielaDiazB <gabriela.dbravo@gmail.com>
2018-11-23 03:27:07 -0500

    Cipher terminado, JS terminado, HTML terminado, CSS cambiado.

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,4 @@
+//función de cifrado de texto
 const cifradoDeTexto =(texto,offset)=>{
   let stringAscii=[];
   let codigo=[];
@@ -12,28 +13,27 @@ const cifradoDeTexto =(texto,offset)=>{
     nuevoCodigo = codigo.join("");  
   };
 return nuevoCodigo;
-//console.log(nuevoCodigo)
 };
 
+//función de decifrado de texto
+const decifradoDeTexto = (texto,offset)=>{
+  let stringAscii=[];
+  let codigo=[];
+  for(let i=0; i<texto.length; i++){
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (texto.charCodeAt(i)>=65 && texto.charCodeAt(i)<=90) {
+        stringAscii.push(((texto.charCodeAt(i)-90-parseInt(offset))%26 + 90));
+        codigo.push(String.fromCharCode(stringAscii[i]));
+    } else if (texto.charCodeAt(i)===32){
+        stringAscii.push(texto.charCodeAt(i));
+        codigo.push(String.fromCharCode(stringAscii[i]));
+    };
+    nuevoCodigo = codigo.join("");
+  };
+  return nuevoCodigo;
+};
 
 window.cipher = {
-  encode: cifradoDeTexto
+  encode: cifradoDeTexto,
+  decode: decifradoDeTexto
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
 
-const buttonStart = document.getElementById("buttonStart");
-buttonStart.addEventListener('click',functionStartApp);
-
-const button1 = document.getElementById("cifradoDeTexto");
-button1.addEventListener('click', cifradoDeTexto);
-
-const button2 = document.getElementById("decifradoDeTexto");
-button2.addEventListener('click', decifradoDeTexto);
-
 document.getElementById("cipherBox").style.display="none";
 document.getElementById("results").style.display="none";
 
-const resultados = document.getElementById("resultados");
-
-let texto = document.getElementById("textBox").value.toUpperCase();
-let offset = document.getElementById("offsetBox").value;
-let nuevoCodigo = [];
-resultados.innerHTML=cipher.encode(texto,offset);
-
+const buttonStart = document.getElementById("buttonStart");
+buttonStart.addEventListener('click',functionStartApp);
 
 function functionStartApp(){
     document.getElementById("start").style.display="none";
@@ -25,55 +11,27 @@ function functionStartApp(){
     document.getElementById("results").style.display="block";
 };
 
+const resultados = document.getElementById("resultados");
 
-
-
-
-
-
-
-/*
-function cifradoDeTexto(){
-    let offset = document.getElementById("offsetBox").value;
-    let texto = document.getElementById("textBox").value.toUpperCase();
-    let stringAscii=[];
-    let codigo=[];
-    let nuevoCodigo=[];
-    for(let i=0; i<texto.length; i++){
-
-        if (texto.charCodeAt(i)>=65 && texto.charCodeAt(i)<=90) {
-            stringAscii.push(((texto.charCodeAt(i)-65+parseInt(offset))%26 + 65));
-            codigo.push(String.fromCharCode(stringAscii[i]));
-        } else if (texto.charCodeAt(i)===32){
-            stringAscii.push(texto.charCodeAt(i));
-            codigo.push(String.fromCharCode(stringAscii[i]));
-        }
-        
-    };
-    nuevoCodigo = codigo.join("");
+const button1 = document.getElementById("cifradoDeTexto");
+button1.addEventListener('click', function(event){
+    event.preventDefault();
+    const texto = document.getElementById("textBox").value.toUpperCase();
+    const offset = document.getElementById("offsetBox").value;
+    const nuevoCodigo = cipher.encode(texto,offset);
     resultados.innerHTML = nuevoCodigo;
-    //console.log(nuevoCodigo)
-};*/ 
+});
 
-function decifradoDeTexto(){
-    let offset = document.getElementById("offsetBox").value;
-    let texto = document.getElementById("textBox").value.toUpperCase();
-    let stringAscii=[];
-    let codigo=[];
-    let nuevoCodigo=[];
-
-    for(let i=0; i<texto.length; i++){
-
-        if (texto.charCodeAt(i)>=65 && texto.charCodeAt(i)<=90) {
-            stringAscii.push(((texto.charCodeAt(i)-90-parseInt(offset))%26 + 90));
-            codigo.push(String.fromCharCode(stringAscii[i]));
-        } else if (texto.charCodeAt(i)===32){
-            stringAscii.push(texto.charCodeAt(i));
-            codigo.push(String.fromCharCode(stringAscii[i]));
-        };
-    nuevoCodigo = codigo.join("");
-    };
+const button2 = document.getElementById("decifradoDeTexto");
+button2.addEventListener('click',function(event){
+    event.preventDefault();
+    const texto = document.getElementById("textBox").value.toUpperCase();
+    const offset = document.getElementById("offsetBox").value;
+    const nuevoCodigo = cipher.decode(texto,offset);
     resultados.innerHTML = nuevoCodigo;
-    
-};
+});
+
+
+
+
 
