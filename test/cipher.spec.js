2aed6baf1b4d1baf1d155f44bed7068a4ae66350
GabrielaDiazB <gabriela.dbravo@gmail.com>
2018-11-28 00:43:45 -0500

    cambios de estilo, posicionamientos de divs

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -21,8 +21,33 @@ describe('cipher', () => {
 
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
   });
+});  
 
- /* describe('cipher.createCipherWithOffset', () => {
+ /*describe('cipher', () => {
+
+  it('debería ser un objeto', () => {
+    assert.equal(typeof cipher, 'object');
+  });
+
+  describe('cipher.encode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.encode, 'function');
+    });
+
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33');
+  });
+
+  describe('cipher.decode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.decode, 'function');
+    });
+
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
+  });
+
+ describe('cipher.createCipherWithOffset', () => {
 
     it('debería ser una función', () => {
       assert.equal(typeof cipher.createCipherWithOffset, 'function');
@@ -32,4 +57,4 @@ describe('cipher', () => {
 
   });*/
 
-});
+
