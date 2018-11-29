3c84fbfbf3941da8ceb13d60f2004676379d81f9
GabrielaDiazB <gabriela.dbravo@gmail.com>
2018-11-29 08:32:51 -0500

    reemplazo de codigo/git no lee

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -21,33 +21,8 @@ describe('cipher', () => {
 
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
   });
-});  
 
- /*describe('cipher', () => {
-
-  it('debería ser un objeto', () => {
-    assert.equal(typeof cipher, 'object');
-  });
-
-  describe('cipher.encode', () => {
-
-    it('debería ser una función', () => {
-      assert.equal(typeof cipher.encode, 'function');
-    });
-
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33');
-  });
-
-  describe('cipher.decode', () => {
-
-    it('debería ser una función', () => {
-      assert.equal(typeof cipher.decode, 'function');
-    });
-
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
-  });
-
- describe('cipher.createCipherWithOffset', () => {
+ /* describe('cipher.createCipherWithOffset', () => {
 
     it('debería ser una función', () => {
       assert.equal(typeof cipher.createCipherWithOffset, 'function');
@@ -57,4 +32,4 @@ describe('cipher', () => {
 
   });*/
 
-
+});
