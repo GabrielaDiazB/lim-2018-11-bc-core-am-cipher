4df104951dc9ecb3244a8c1cc55503cdb3c4af84
GabrielaDiazB <gabriela.dbravo@gmail.com>
2018-11-29 11:44:27 -0500

    Test terminado,cambio en el cipher(funcion de espacios), cambios de posicion en la hoja de estilos

--- src/cipher.js
+++ src/cipher.js
@@ -2,37 +2,33 @@
 const cifradoDeTexto =(texto,offset)=>{
   let stringAscii=[];
   let codigo=[];
-  let nuevoCodigo=[];
   for(let i=0; i<texto.length; i++){
    if (texto.charCodeAt(i)>=65 && texto.charCodeAt(i)<=90) {
     stringAscii.push(((texto.charCodeAt(i)-65+parseInt(offset))%26 + 65));
+    codigo.push(String.fromCharCode(stringAscii[i])); 
+  } else if (texto.charCodeAt(i)===32){
+    stringAscii.push(texto.charCodeAt(i));
     codigo.push(String.fromCharCode(stringAscii[i]));
-   } else if (texto.charCodeAt(i)===32){
-      stringAscii.push(texto.charCodeAt(i));
-      codigo.push(String.fromCharCode(stringAscii[i]));
-    }
-    nuevoCodigo = codigo.join("");  
   }
-return nuevoCodigo;
+  
+   }
+  return codigo.join("");
 };
 
 //función de decifrado de texto
 const descifradoDeTexto = (texto,offset)=>{
   let stringAscii=[];
   let codigo=[];
-  let nuevoCodigo=[];
   for(let i=0; i<texto.length; i++){
-
     if (texto.charCodeAt(i)>=65 && texto.charCodeAt(i)<=90) {
-        stringAscii.push(((texto.charCodeAt(i)-90-parseInt(offset))%26 + 90));
-        codigo.push(String.fromCharCode(stringAscii[i]));
+      stringAscii.push(((texto.charCodeAt(i)-90-parseInt(offset))%26 + 90));
+      codigo.push(String.fromCharCode(stringAscii[i]));    
     } else if (texto.charCodeAt(i)===32){
-        stringAscii.push(texto.charCodeAt(i));
-        codigo.push(String.fromCharCode(stringAscii[i]));
-    }
-    nuevoCodigo = codigo.join("");
+      stringAscii.push(texto.charCodeAt(i));
+      codigo.push(String.fromCharCode(stringAscii[i]));
   }
-  return nuevoCodigo;
+  }
+  return codigo.join("");
 };
 
 window.cipher = {
