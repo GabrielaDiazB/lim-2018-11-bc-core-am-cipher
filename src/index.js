c23e04fc8ca806bff4c257921575e73d71ca0b8c
GabrielaDiazB <gabriela.dbravo@gmail.com>
2018-12-04 12:12:32 -0500

    correcćión de nombramiento de id's en html, js y css; correción del src de imgs en HTML

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 
-document.getElementById("cipherBox").style.display="none";
+document.getElementById("cipher-box").style.display="none";
 document.getElementById("results").style.display="none";
 
-const buttonStart = document.getElementById("buttonStart");
+const buttonStart = document.getElementById("button-start");
 buttonStart.addEventListener('click',functionStartApp);
 
 function functionStartApp(){
     document.getElementById("start").style.display="none";
-    document.getElementById("cipherBox").style.display="block";
+    document.getElementById("cipher-box").style.display="block";
     document.getElementById("results").style.display="block";
 }
 
@@ -15,27 +15,27 @@ const resultados = document.getElementById("resultados");
 
 
 //Botón para Cifrar+Evento
-const button1 = document.getElementById("cifradoDeTexto");
+const button1 = document.getElementById("cifrado-de-texto");
 button1.addEventListener('click', function(event){
     event.preventDefault();
-    const text = document.getElementById("textBox").value;
-    const offset = document.getElementById("offsetBox").value;
+    const text = document.getElementById("text-box").value;
+    const offset = document.getElementById("offset-box").value;
     const code = cipher.encode(offset, text);
     resultados.innerHTML = code;
 });
 
 //Botón para Descifrar+Evento
-const button2 = document.getElementById("descifradoDeTexto");
+const button2 = document.getElementById("descifrado-de-texto");
 button2.addEventListener('click',function(event){
     event.preventDefault();
-    const text = document.getElementById("textBox").value;
-    const offset = document.getElementById("offsetBox").value;
+    const text = document.getElementById("text-box").value;
+    const offset = document.getElementById("offset-box").value;
     const code = cipher.decode(offset, text);
     resultados.innerHTML = code;
 });
 
 //Botón para copiar el resultado
-const button3 = document.getElementById('copyBtn');
+const button3 = document.getElementById('copy-btn');
 
 button3.addEventListener('click', () => {
    const copyText = document.getElementById("resultados");
